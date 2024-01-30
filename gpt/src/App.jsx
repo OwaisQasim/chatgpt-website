1730de5c849c8ab2a7c826a9b8c5e318fa197e30
iamrfowaisqasim <rfowaisqasim@gmail.com>
2024-01-30 13:26:47 +0500

    complete responsive navigation bar is done

--- gpt/src/App.jsx
+++ gpt/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { Possibility, Header, Whatgpt3, Features, Blog, Footer } from './containers'
+import { CTA, Brand, Navbar } from './components'
+import './App.css'
 
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className='App'>
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <Whatgpt3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
