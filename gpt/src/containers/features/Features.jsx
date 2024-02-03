ae1e063012620605c9c39a0d590c22c7572d547a
iamrfowaisqasim <rfowaisqasim@gmail.com>
2024-02-03 21:53:02 +0500

    edited whatgpt3

--- gpt/src/containers/features/Features.jsx
+++ gpt/src/containers/features/Features.jsx
@@ -3,7 +3,12 @@ import './features.css'
 
 const Features = () => {
     return (
-        <div>Features</div>
+        <div className='gpt3__features-container section__padding'>
+            <div className="gpt3__features-heading">
+                <h1>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+                <p>Request Early Access to Get Started</p>
+            </div>
+        </div>
     )
 }
 
