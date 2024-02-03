9296122ad3753843042fc47121809f5c7b93285e
iamrfowaisqasim <rfowaisqasim@gmail.com>
2024-02-03 21:10:15 +0500

    whatisGPT-3 section is done and now moving on to Features

--- gpt/src/containers/header/Header.jsx
+++ gpt/src/containers/header/Header.jsx
@@ -5,9 +5,9 @@ import ai from '../../assets/ai.png'
 
 const Header = () => {
     return (
-        <div className='gpt3__header section__padding'>
+        <div className='gpt3__header section__padding' id='Header'>
             <div className="gpt3__header-content">
-                <h1>
+                <h1 className='gradient__text'>
                     Let's Build Something amazing with GPT-3 Open AI
                 </h1>
                 <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
